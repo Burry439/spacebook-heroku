6799cde4e9836422a8c21fdec7745ec97279a1e9
burry <burry439@gmail.com>
2017-10-01 13:30:06 +0300

    changed uri

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/spacebook', function() {
+mongoose.connect(process.env.BURRYLAB_URI || 'mongodb://localhost/spacebook', function() {
   console.log("DB connection established!!!");
 })
 
@@ -99,6 +99,6 @@ app.delete('/posts/:id/comments/:commentID', function(req, res){
   })
 
 
-app.listen(8000, function() {
+app.listen(process.env.PORT||8000, function() {
   console.log("what do you want from me! get me on 8000 ;-)");
 });
